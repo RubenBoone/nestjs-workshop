fc42b9684e5ed3af7b056f0ee90cb5a3e9a4bf15
Ruben Boone <r0785519@student.thomasmore.be>
2021-12-01 14:59:04 +0100

    add MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TodoModule } from './todo/todo.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [TodoModule, MongooseModule.forRoot('mongodb+srv://nestjs:nestjs123@cluster0.ii8g6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')],
   controllers: [AppController],
   providers: [AppService],
 })
