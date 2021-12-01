e67f8ce04d6a621e9ace7be040a7782431fdb795
Ruben Boone <r0785519@student.thomasmore.be>
2021-12-01 15:13:16 +0100

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo App')
+    .setDescription('Todo API documentation')
+    .setVersion('0.1')
+    .addTag('todos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
